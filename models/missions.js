a6e52d67282f07bb0190fd7791a388e1420a978b
Josh Hepworth <jrhepworth12@gmail.com>
2018-10-08 23:25:30 -0500

    added image for curiosity

--- models/missions.js
+++ models/missions.js
@@ -4,14 +4,14 @@ const marsMissions = [
     launchDate: "26 Nov 2011",
     operator: "NASA",
     missionType: "Rover",
-    img: ""
+    img: "http://www.wallpaperbetter.com/wallpaper/15/951/740/curiosity-rover-mars-nasa-alien-landscape-robot-machine-hd-1080P-wallpaper-middle-size.jpg"
   },
   {
     name: "Opportunity",
     launchDate: "8 Jul 2003",
     operator: "NASA",
     missionType: "Rover",
-    img: ""
+    img: "https://img.purch.com/w/660/aHR0cDovL3d3dy5zcGFjZS5jb20vaW1hZ2VzL2kvMDAwLzA3OC80MDQvb3JpZ2luYWwvcm92ZXIyLmpwZw=="
   },
   {
     name: "Spirit",
