d7a690fd5899ba740386d0b5965eaffec8180fb6
Filip Tatarnik <ftatarnik@gmail.com>
2018-10-08 20:54:45 -0500

    MVP is done, bonus/hungry time

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const app = express();
 
+const Missions = require('./models/marsMissions')
 // run `npm install` to install dependencies in package.json
 
 // * Your mission is to complete the app
@@ -22,55 +23,40 @@ const app = express();
 // PORT
 const port = 3000;
 
-// DATA - put into marsMissions.js file inside of a models folder, for module.exports
-// remember to require it in the server
-const marsMissions = [
-  {
-    name: "Curiosity",
-    launchDate: "26 Nov 2011",
-    operator: "NASA",
-    missionType: "Rover",
-    img: ""
-  },
-  {
-    name: "Opportunity",
-    launchDate: "8 Jul 2003",
-    operator: "NASA",
-    missionType: "Rover",
-    img: ""
-  },
-  {
-    name: "Spirit",
-    launchDate: "10 Jun 2003",
-    operator: "NASA",
-    missionType: "Rover",
-    img: ""
-  },
-  {
-    name: "Sojourner",
-    launchDate: "4 Dec 1996",
-    operator: "NASA",
-    missionType: "Rover",
-    img: ""
-  },
-  {
-    name: "Rosetta",
-    launchDate: "2 Mar 2004",
-    operator: "ESA",
-    missionType: "Gravity Assist",
-    img: ""
-  }
-];
+
+// app.get('./models/fil', (req, res)=>{
+//   res.send('fil is pretty coo')
+// })
+
+
+
+
+
 
 // INDEX Route
 // send data to 'missions/index.ejs' view
 // the view should display just the names of each mission
 // display the mission names as <li> in a <ul> with the class name "missions"
+app.get('/models/', (req, res)=>{
+  res.render('index.ejs', {
+    Curiosity: Missions[0].name,
+    Opportunity: Missions[1].name,
+    Spirit: Missions[2].name,
+    Sojourner: Missions[3].name,
+    Rosetta: Missions[4].name
+  })
+})
 
 // SHOW Route
 // send data to 'missions/show.ejs' view
 // the view should display all the data for a single mission
 
+app.get('/models/:index', (req, res)=>{
+  console.log(req.params);
+    res.render('show.ejs', {
+      mission: Missions[req.params.index]
+  });
+});
 
 
 // LISTENER
