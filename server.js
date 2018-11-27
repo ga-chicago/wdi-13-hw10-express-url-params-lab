568f6d6a2a3f51a42cadce0634aea5796a5c12cf
Ashley Fueger <amfueger@gmail.com>
2018-11-27 02:18:52 -0600

    final commit

--- server.js
+++ server.js
@@ -7,45 +7,23 @@ const MarsMissions = require('./models/missions');
 // * Your mission is to complete the app
 // * The app will need routes for index and show
 
-//Index route - missions
+//INDEX
 app.get('/missions', (req, res) => {
-  res.send(MarsMissions);
+  res.render('../views/index.ejs', {
+    marsMissions: MarsMissions
+  })
 });
 
-//show route - missions
+//SHOW
 app.get('/missions/:id', (req, res) => {
-  res.render('show.ejs', {
+  res.render('../views/show.ejs', {
     marsMissions: MarsMissions[req.param.id]
   })
 });
-// * The app will need views for index and show
-
-// app.set('views', '');
-// app.get('/:id', (req, res) => {
-
-// });
-//
-// * MAIN GOAL:
-// * User should be able to click on a mission’s name on the index page, and be taken to that mission’s show page
-//
-// * Bonus/Hungry for More: add images to the data and have them display (google how)
-// * Bonus/Hungry for More: add static css to style the pages (google how)
 
 
 // PORT
 const port = 3000;
-
-
-
-// INDEX Route
-// send data to 'missions/index.ejs' view
-// the view should display just the names of each mission
-// display the mission names as <li> in a <ul> with the class name "missions"
-
-// SHOW Route
-// send data to 'missions/show.ejs' view
-// the view should display all the data for a single mission
-
 
 
 // LISTENER
